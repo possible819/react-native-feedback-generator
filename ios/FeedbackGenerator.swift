5ce693caef22449e15a6eeea122dd03c76943f5e
Jay Lee <jaylee.possible@gmail.com>
2022-05-06 03:38:28 +0900

    fix: wrong condition to find right type

--- ios/FeedbackGenerator.swift
+++ ios/FeedbackGenerator.swift
@@ -2,83 +2,79 @@ import UIKit
 
 @objc(CalendarManager)
 class CalendarManager: NSObject {
-  @objc static func requiresMainQueueSetup() -> Bool {
-    return false
-  }
-  
-  @objc(impactFeedback:)
-  func impactFeedback(_ type: NSNumber) -> Void {
-    var style: UIImpactFeedbackGenerator.FeedbackStyle;
-    
-    switch (type.intValue) {
-    case UIImpactFeedbackGenerator.FeedbackStyle.medium.rawValue:
-      style  = UIImpactFeedbackGenerator.FeedbackStyle.medium;
-      
-    case UIImpactFeedbackGenerator.FeedbackStyle.heavy.rawValue:
-      style = UIImpactFeedbackGenerator.FeedbackStyle.heavy;
-      
-    case UIImpactFeedbackGenerator.FeedbackStyle.light.rawValue:
-      style = UIImpactFeedbackGenerator.FeedbackStyle.light;
-      
-    case UIImpactFeedbackGenerator.FeedbackStyle.soft.rawValue:
-      if @available(iOS 13.0.0 *) {
-        style = UIImpactFeedbackGenerator.FeedbackStyle.soft;
-      } else {
-        style  = UIImpactFeedbackGenerator.FeedbackStyle.medium;
-      }
-      
-    case UIImpactFeedbackGenerator.FeedbackStyle.rigid.rawValue:
-      if @available(iOS 13.0.0 *) {
-        style = UIImpactFeedbackGenerator.FeedbackStyle.rigid
-      } else {
-        style = UIImpactFeedbackGenerator.FeedbackStyle.medium;
-      }
-      
-    default:
-      style = UIImpactFeedbackGenerator.FeedbackStyle.medium;
+    @objc static func requiresMainQueueSetup() -> Bool {
+        return false
     }
     
-    UIImpactFeedbackGenerator(style: style).impactOccurred();
-  }
-  
-  @objc(selectionFeedback)
-  func selectionFeedback() -> Void {
-    UISelectionFeedbackGenerator.init().selectionChanged();
-  }
-  
-  @objc(notificationFeedback:)
-  func notificationFeedback(_ type: NSNumber) -> Void {
-    var notificationType: UINotificationFeedbackGenerator.FeedbackType
-    
-    switch (type.intValue) {
-    case UINotificationFeedbackGenerator.FeedbackType.success.rawValue:
-      notificationType = UINotificationFeedbackGenerator.FeedbackType.success;
-      
-    case UINotificationFeedbackGenerator.FeedbackType.warning.rawValue:
-      notificationType = UINotificationFeedbackGenerator.FeedbackType.warning;
-      
-    case UINotificationFeedbackGenerator.FeedbackType.error.rawValue:
-      notificationType = UINotificationFeedbackGenerator.FeedbackType.error;
-      
-    default:
-      notificationType = UINotificationFeedbackGenerator.FeedbackType.success;
+    @objc(impactFeedback:)
+    func impactFeedback(_ type: NSNumber) -> Void {
+        var style: UIImpactFeedbackGenerator.FeedbackStyle;
+        
+        switch (type.intValue) {
+        case UIImpactFeedbackGenerator.FeedbackStyle.medium.rawValue:
+            style  = UIImpactFeedbackGenerator.FeedbackStyle.medium;
+            
+        case UIImpactFeedbackGenerator.FeedbackStyle.heavy.rawValue:
+            style = UIImpactFeedbackGenerator.FeedbackStyle.heavy;
+            
+        case UIImpactFeedbackGenerator.FeedbackStyle.light.rawValue:
+            style = UIImpactFeedbackGenerator.FeedbackStyle.light;
+            
+        default:
+            if #available(iOS 13.0.0, *) {
+                if (type.intValue == UIImpactFeedbackGenerator.FeedbackStyle.rigid.rawValue) {
+                    style = UIImpactFeedbackGenerator.FeedbackStyle.rigid
+                } else if (type.intValue == UIImpactFeedbackGenerator.FeedbackStyle.rigid.rawValue) {
+                    style = UIImpactFeedbackGenerator.FeedbackStyle.rigid
+                } else {
+                    style = UIImpactFeedbackGenerator.FeedbackStyle.medium;
+                }
+            } else {
+                style = UIImpactFeedbackGenerator.FeedbackStyle.medium;
+            }
+        }
+        
+        UIImpactFeedbackGenerator(style: style).impactOccurred();
     }
     
-    UINotificationFeedbackGenerator.init().notificationOccurred(notificationType)
-  }
-  
-  
-  @objc
-  func constantsToExport() -> [String: Int]! {
-    return [
-      "ImpactFeedbackStyleLight": 0,
-      "ImpactFeedbackStyleMedium": 1,
-      "ImpactFeedbackStyleHeavy": 2,
-      "ImpactFeedbackStyleSoft": 3,
-      "ImpactFeedbackStyleRigid": 4,
-      "NotificationFeedbackStyleSuccess": 0,
-      "NotificationFeedbackStyleWarning": 1,
-      "NotificationFeedbackStyleError": 2
-    ]
-  }
+    @objc(selectionFeedback)
+    func selectionFeedback() -> Void {
+        UISelectionFeedbackGenerator.init().selectionChanged();
+    }
+    
+    @objc(notificationFeedback:)
+    func notificationFeedback(_ type: NSNumber) -> Void {
+        var notificationType: UINotificationFeedbackGenerator.FeedbackType
+        
+        switch (type.intValue) {
+        case UINotificationFeedbackGenerator.FeedbackType.success.rawValue:
+            notificationType = UINotificationFeedbackGenerator.FeedbackType.success;
+            
+        case UINotificationFeedbackGenerator.FeedbackType.warning.rawValue:
+            notificationType = UINotificationFeedbackGenerator.FeedbackType.warning;
+            
+        case UINotificationFeedbackGenerator.FeedbackType.error.rawValue:
+            notificationType = UINotificationFeedbackGenerator.FeedbackType.error;
+            
+        default:
+            notificationType = UINotificationFeedbackGenerator.FeedbackType.success;
+        }
+        
+        UINotificationFeedbackGenerator.init().notificationOccurred(notificationType)
+    }
+    
+    
+    @objc
+    func constantsToExport() -> [String: Int]! {
+        return [
+            "ImpactFeedbackStyleLight": 0,
+            "ImpactFeedbackStyleMedium": 1,
+            "ImpactFeedbackStyleHeavy": 2,
+            "ImpactFeedbackStyleSoft": 3,
+            "ImpactFeedbackStyleRigid": 4,
+            "NotificationFeedbackStyleSuccess": 0,
+            "NotificationFeedbackStyleWarning": 1,
+            "NotificationFeedbackStyleError": 2
+        ]
+    }
 }
